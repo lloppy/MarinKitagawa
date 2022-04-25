be3dfa1ddd02090569d3cf80ba94cc91c82ad873
lloppy <ankudinovapol@gmail.com>
2022-04-25 20:46:08 +0500

    add moves

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -19,10 +19,12 @@ suspend fun main() = Korge(width = 512, height = 512, bgcolor = Colors["#2b2b2b"
 	val adventurerSprites: Atlas = resourcesVfs["adventurer.xml"].readAtlas()
 
 	val idleAnimation:SpriteAnimation = adventurerSprites.getSpriteAnimation(prefix = "idle")
-	val walkReadyAnimation:SpriteAnimation = adventurerSprites.getSpriteAnimation(prefix = "walk")
-	val walkBackReadyAnimation:SpriteAnimation = adventurerSprites.getSpriteAnimation(prefix = "walk")
+	val walkReadyAnimation:SpriteAnimation = adventurerSprites.getSpriteAnimation(prefix = "walk-")
+	val walkBackReadyAnimation:SpriteAnimation = adventurerSprites.getSpriteAnimation(prefix = "walkback-")
 	val downReadyAnimation:SpriteAnimation = adventurerSprites.getSpriteAnimation(prefix = "down")
+	val rowJumpAnimation:SpriteAnimation = adventurerSprites.getSpriteAnimation(prefix = "rowjump")
 	val attackReadyAnimation:SpriteAnimation = adventurerSprites.getSpriteAnimation(prefix = "attack-ready")
+	val walkintoAnimation:SpriteAnimation = adventurerSprites.getSpriteAnimation(prefix = "walkinto-")
 
 	val adventurer: Sprite = sprite(idleAnimation).scale(4.0).xy(100.0, 100.0)
 	adventurer.playAnimationLooped(spriteDisplayTime = 200.milliseconds)
@@ -32,14 +34,18 @@ suspend fun main() = Korge(width = 512, height = 512, bgcolor = Colors["#2b2b2b"
 	}
 
 	addUpdater {
-		if (views.input.keys.pressing(Key.A)) {
+		if (views.input.keys.pressing(Key.X)) {
 			playerStatusPlayAnimation(adventurer, attackReadyAnimation, PlayerStatus.ATTACK_READY)
-		} else if (views.input.keys.pressing(Key.RIGHT)) {
+		} else if (views.input.keys.pressing(Key.E)) {
+			playerStatusPlayAnimation(adventurer, rowJumpAnimation, PlayerStatus.DOWN)
+		} else if (views.input.keys.pressing(Key.RIGHT) or views.input.keys.pressing(Key.D)) {
 			playerStatusPlayAnimation(adventurer, walkReadyAnimation, PlayerStatus.WALK)
-		} else if (views.input.keys.pressing(Key.LEFT)) {
+		} else if (views.input.keys.pressing(Key.LEFT) or views.input.keys.pressing(Key.A)) {
 			playerStatusPlayAnimation(adventurer, walkBackReadyAnimation, PlayerStatus.WALKBACK)
-		} else if (views.input.keys.pressing(Key.DOWN)) {
+		} else if (views.input.keys.pressing(Key.DOWN) or views.input.keys.pressing(Key.S)) {
 			playerStatusPlayAnimation(adventurer, downReadyAnimation, PlayerStatus.DOWN)
+		} else if (views.input.keys.pressing(Key.UP) or views.input.keys.pressing(Key.W)) {
+			playerStatusPlayAnimation(adventurer, walkintoAnimation, PlayerStatus.WALKINTO)
 		} else {
 			playerStatusPlayAnimation(adventurer, idleAnimation, PlayerStatus.IDLE)
 		}
@@ -54,6 +60,14 @@ fun playerStatusPlayAnimation(sprite: Sprite, animation: SpriteAnimation, paramP
 			sprite.playAnimation(animation)
 		}
 		PlayerStatus.WALK -> {
+			sprite.playAnimation(animation)
+			sprite.x += 2
+		}
+		PlayerStatus.WALKINTO -> {
+			sprite.playAnimation(animation)
+			sprite.y -= 2
+		}
+		PlayerStatus.ROWJUMP -> {
 			sprite.playAnimation(animation)
 			sprite.x += 2
 		}
